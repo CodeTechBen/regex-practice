5518aa2d0088d88c8857cae1a07e62d55d09677b
CodeTechBen <benjamin-smith@live.com>
2024-12-17 16:29:11 +0000

    extract numbers

---
+++ easy/chall-3/README.md
@@ -0,0 +1,2 @@
+Extract all numbers from the text.
+There are 12 apples, 3 bananas, and 45 oranges.
---
+++ easy/chall-3/chall_3.py
@@ -0,0 +1,3 @@
+import re
+
+print(re.findall(r'[0-9]+',"There are 12 apples, 3 bananas, and 45 oranges."))
