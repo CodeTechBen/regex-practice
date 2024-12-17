f07a00567e00cf1bcf532aade0b2dfa8cbf57a9f
CodeTechBen <benjamin-smith@live.com>
2024-12-17 17:28:18 +0000

    word boundery

--- intermediate/chall-1/README.md
+++ intermediate/chall-1/README.md
@@ -1,1 +1,3 @@
 Match all words that contain only lowercase letters.
+
+text: regex is FUN and very useful in Coding.
---
+++ intermediate/chall-1/chall_1.py
@@ -0,0 +1,5 @@
+import re
+
+result = re.findall(r'\b[a-z]+\b', "regex is FUN and very useful in Coding")
+
+print(result)
